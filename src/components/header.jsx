931834685f08f89808f17bacb5c297c0da93fa48
TranSon <tson113355@gmail.com>
2024-08-19 18:43:08 +0700

    chore: Update responsive styles for smaller screen sizes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,35 @@
 import {
     NavLink, useNavigate
 } from 'react-router-dom'
-
+import React, {useState} from 'react';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     const navigate = useNavigate();
 
     const handleLogoClick = () => {
         navigate('/');
     }
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <div className="header">
+            <button className="menu-button" onClick={toggleMenu}>
+                ☰
+            </button>
             <div className="logo" onClick={handleLogoClick}>
                 <img height={90} src="http://kamiautobot.com/wp-content/uploads/2024/07/Kamiautubot-logo-ngang.png" alt="logo" />
             </div>
-            <div>
+            <div className='header-link'>
+                <NavLink to="/" className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}>Trang chủ</NavLink>
+                <NavLink to="/rok" className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}>Rise Of Kingdom Bot</NavLink>
+                <NavLink to="/privacy" className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}>Chính sách bảo mật và đền bù</NavLink>
+                <a href="https://wbs.minarok.com/" className={`nav-link`}>Đăng nhập Bot</a>
+            </div>
+            <div className={`header-link modal-header-link ${menuOpen? 'open' : ''}`}>
                 <NavLink to="/" className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}>Trang chủ</NavLink>
                 <NavLink to="/rok" className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}>Rise Of Kingdom Bot</NavLink>
                 <NavLink to="/privacy" className={({ isActive }) => `nav-link ${isActive ? 'active-link' : ''}`}>Chính sách bảo mật và đền bù</NavLink>
